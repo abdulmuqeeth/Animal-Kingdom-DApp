a81ffd71ec84b6ff09108365803e9870343e72ea
abdulmuqeethmohammed <amoham62@uic.edu>
2019-06-26 01:15:41 -0500

    Adding files

---
+++ build/precache-manifest.05fdbd71ba82995e92dfa7c80c333510.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88d9e75ad6942571162960130b9cd14b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "982b20c172666c9e74dc",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "512b01e6cb1a4d412dc8",
+    "url": "/static/css/main.eb882395.chunk.css"
+  },
+  {
+    "revision": "982b20c172666c9e74dc",
+    "url": "/static/js/2.09ac52cb.chunk.js"
+  },
+  {
+    "revision": "512b01e6cb1a4d412dc8",
+    "url": "/static/js/main.a92b6573.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
